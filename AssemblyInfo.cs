5ce96b60943cc749dc5a83b7fdb9f222cb0f3754
chienhao10 <huangjianhao_10@hotmail.com>
2017-01-10 19:55:10 -0500

    1

---
+++ AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SharpShooter Reworked")]
+[assembly: AssemblyDescription("SharpShooter Reworked by xcsoft")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xcsoft")]
+[assembly: AssemblyProduct("SharpShooter")]
+[assembly: AssemblyCopyright("Copyright © xcsoft. All rights reserved.")]
+[assembly: AssemblyTrademark("xcsoft")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible을 false로 설정하면 이 어셈블리의 형식이 COM 구성 요소에 
+// 표시되지 않습니다.  COM에서 이 어셈블리의 형식에 액세스하려면 
+// 해당 형식에 대해 ComVisible 특성을 true로 설정하세요.
+
+[assembly: ComVisible(false)]
+
+// 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
+
+[assembly: Guid("fe56cef8-9134-4ff5-ae2c-e9a81cb56efe")]
+
+// 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
+//
+//      주 버전
+//      부 버전 
+//      빌드 번호
+//      수정 버전
+//
+// 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 번호가 자동으로 
+// 지정되도록 할 수 있습니다.
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("6.23.0.23")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
